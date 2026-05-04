d772c1f7ad3c9bc6c0f5c0df62be9619012e243c
Keita Takahashi <takanenokei@gmail.com>
2026-05-04 22:22:57 +0900

    refactor: use destructuring assignment for input parsing

--- atcoder/abs/welcome-to-atcoder.js
+++ atcoder/abs/welcome-to-atcoder.js
@@ -2,9 +2,7 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
 
 const a = Number(lines[0]);
-const tmp = lines[1].split(' ');
-const b = Number(tmp[0]);
-const c = Number(tmp[1]);
+const [b, c] = lines[1].split(' ').map(Number);
 const s = lines[2];
 
 console.log(`${a + b + c} ${s}`);
