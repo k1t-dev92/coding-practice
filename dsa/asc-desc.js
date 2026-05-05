ea0542a3b7c81fb2831e326227c4b30be534e1ad
Keita Takahashi <takanenokei@gmail.com>
2026-05-05 13:58:45 +0900

    refactor: replace parseInt with Number for consistency

--- dsa/asc-desc.js
+++ dsa/asc-desc.js
@@ -1,7 +1,7 @@
 const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
 
-const n = parseInt(lines[0]);
+const n = Number(lines[0]);
 const numbers = lines[1].split(' ').map(Number);
 
 let tmp;
