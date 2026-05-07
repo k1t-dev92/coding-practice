025e66e295bdf143fa4995f0b3acd550e5579458
Keita Takahashi <takanenokei@gmail.com>
2026-05-07 22:50:40 +0900

    feat: add integer-array-newline-input-2 solition

---
+++ paiza-levelup/c-rank/integer-array-newline-input-2.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const [n, b, c] = lines[0].split(' ').map(Number);
+const a = lines.slice(1).map(Number);
+
+console.log(n + 1);
+console.log(b + 1);
+console.log(c + 1);
+for (let i = 0; i < a.length; i++) {
+  console.log(a[i] + 1);
+}
