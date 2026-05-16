6c02ca20ada06ad05b6121ca6499ec112e8375d2
Keita Takahashi <takanenokei@gmail.com>
2026-05-16 23:03:00 +0900

    refactor: avoid mutating numbers array

--- dsa/asc-desc-builtin.js
+++ dsa/asc-desc-builtin.js
@@ -3,8 +3,8 @@ const lines = input.trim().split('\n');
 
 const numbers = lines[0].split(' ').map(Number);
 
-const asc = numbers.sort((a, b) => a - b);
+const asc = [...numbers].sort((a, b) => a - b);
 console.log(asc.join(' '));
 
-const desc = numbers.sort((a, b) => b - a);
+const desc = [...numbers].sort((a, b) => b - a);
 console.log(desc.join(' '));
