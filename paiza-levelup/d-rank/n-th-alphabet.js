976d4626e67f91c2ea064f8db0eec8a3b030ae1c
Keita Takahashi <takanenokei@gmail.com>
2026-05-18 20:45:38 +0900

    refactor: replace magic number 64 with charCodeAt expression

--- paiza-levelup/d-rank/n-th-alphabet.js
+++ paiza-levelup/d-rank/n-th-alphabet.js
@@ -2,4 +2,4 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
 
 const n = Number(lines[0]);
-console.log(String.fromCharCode(n + 64));
+console.log(String.fromCharCode('A'.charCodeAt(0) + n - 1));
