a2432b99a6d0e92c1abfd665131b4e94cf278305
Keita Takahashi <takanenokei@gmail.com>
2026-05-20 23:50:06 +0900

    refactor: simplify leap-year logic into single expression

--- dsa/leap-year.js
+++ dsa/leap-year.js
@@ -3,12 +3,5 @@ const lines = input.trim().split('\n');
 
 const year = Number(lines[0]);
 
-if (year % 4 !== 0) {
-  console.log('Common Year');
-} else if (year % 100 !== 0) {
-  console.log('Leap Year');
-} else if (year % 400 !== 0) {
-  console.log('Common Year');
-} else {
-  console.log('Leap Year');
-}
+const isLeap = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+console.log(isLeap ? 'Leap Year' : 'Common Year');
