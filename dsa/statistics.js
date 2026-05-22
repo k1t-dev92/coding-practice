0f7cc48b77e994ac580656431e2c28cac3303876
Keita Takahashi <takanenokei@gmail.com>
2026-05-22 21:53:10 +0900

    fix: avoid NaN in standard deviation from floating-point cancellation

--- dsa/statistics.js
+++ dsa/statistics.js
@@ -4,14 +4,18 @@ const lines = input.trim().split('\n');
 const numbers = lines[0].split(' ').map(Number);
 
 let sum = 0;
-let sumOfSquares = 0;
+let sumOfSquaredDeviations = 0;
 for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i];
-  sumOfSquares += numbers[i] * numbers[i];
 }
 
 const average = sum / numbers.length;
-const variance = sumOfSquares / numbers.length - average * average;
+
+for (let i = 0; i < numbers.length; i++) {
+  sumOfSquaredDeviations += (numbers[i] - average) * (numbers[i] - average);
+}
+
+const variance = sumOfSquaredDeviations / numbers.length;
 const std = Math.sqrt(variance);
 
 console.log(average);
