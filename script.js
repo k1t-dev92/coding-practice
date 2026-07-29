0db5d93020c77bf65fa796cc30599537ae450dae
Keita Takahashi <takanenokei@gmail.com>
2026-07-30 08:45:44 +0900

    feat: add input parsing example

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf-8');
+const lines = input.trim().split('\n');
+
+const n = Number(lines[0]);
+const [a, b] = lines[1].split(' ').map(Number);
+
+console.log(n);
+console.log(a, b);
