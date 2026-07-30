b057f7cef31109690ff17f4b79a35579744f4706
Keita Takahashi <takanenokei@gmail.com>
2026-07-30 23:44:50 +0900

    feat: add recurrence-2term-special-1 solution

---
+++ paiza-levelup/b-rank/recurrence-2term-special-1.js
@@ -0,0 +1,16 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf-8');
+const lines = input.trim().split('\n');
+
+const [x, d1, d2, k] = lines[0].split(' ').map(Number);
+const a = new Array(k + 1);
+a[1] = x;
+
+for (let i = 2; i <= k; i++) {
+  if (i % 2 !== 0) {
+    a[i] = a[i - 1] + d1;
+  } else {
+    a[i] = a[i - 1] + d2;
+  }
+}
+
+console.log(a[k]);
