1801848b61572e522a43e9e63d863e416f798baa
Keita Takahashi <takanenokei@gmail.com>
2026-08-02 23:44:04 +0900

    feat: add next-food solution

---
+++ paiza-levelup/d-rank/next-food.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const data = lines[0].split(' ').map(Number);
+
+if ((data[0] >= 19 && data[0] <= 23) || (data[0] >= 0 && data[0] < 7)) {
+  console.log('breakfast');
+} else if (data[0] >= 7 && data[0] < 13) {
+  console.log('lunch');
+} else if (data[0] >= 13 && data[0] < 19) {
+  console.log('dinner');
+}
