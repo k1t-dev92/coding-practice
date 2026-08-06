e0bad3ca2629799d59dd014b4dee9cb3f7098f04
Keita Takahashi <takanenokei@gmail.com>
2026-08-06 10:45:18 +0900

    feat: add balanced-brackets solution

---
+++ paiza-levelup/algorithms/stack-queue/balanced-brackets.js
@@ -0,0 +1,28 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const n = Number(lines[0]);
+const s = lines[1];
+
+const stack = [];
+let isValid = true;
+
+for (let i = 0; i < n; i++) {
+  if (s[i] === '(') {
+    stack.push(s[i]);
+  } else {
+    if (stack.length === 0) {
+      isValid = false;
+      break;
+    }
+    stack.pop();
+  }
+}
+
+if (stack.length !== 0) {
+  isValid = false;
+}
+
+console.log(isValid ? 'Yes' : 'No');
+
+// https://paiza.jp/works/mondai/stack_queue/stack_queue__practice_step4
