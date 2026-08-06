3cd2f4f9694ab918d0c9634713b9c480919d5bbd
Keita Takahashi <takanenokei@gmail.com>
2026-08-06 09:52:22 +0900

    feat: add reverse-polish-notation solution

---
+++ paiza-levelup/algorithms/stack-queue/reverse-polish-notation.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const n = Number(lines[0]);
+const a = lines[1].split(' ');
+
+const stack = [];
+
+for (let i = 0; i < n; i++) {
+  if (a[i] === '+' || a[i] === '-') {
+    const right = stack.pop();
+    const left = stack.pop();
+    stack.push(a[i] === '+' ? left + right : left - right);
+  } else {
+    stack.push(Number(a[i]));
+  }
+}
+
+console.log(stack.pop());
+
+// https://paiza.jp/works/mondai/stack_queue/stack_queue__practice_step3
