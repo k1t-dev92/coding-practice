a1e373610a38b9dd882c2f446c10d77aef396472
Keita Takahashi <takanenokei@gmail.com>
2026-08-07 15:45:37 +0900

    feat: add lower-bound solution

---
+++ paiza-levelup/binary-search/lower-bound.js
@@ -0,0 +1,31 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const n = Number(lines[0]);
+const a = lines[1].split(' ').map(Number);
+const q = Number(lines[2]);
+
+a.sort((x, y) => x - y);
+
+const answers = [];
+for (let i = 0; i < q; i++) {
+  const k = Number(lines[i + 3]);
+  answers.push(n - binarySearch(a, n, k));
+}
+console.log(answers.join('\n'));
+
+function binarySearch(a, n, k) {
+  let left = 0;
+  let right = n;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (a[mid] < k) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return right;
+}
+
+// https://paiza.jp/works/mondai/binary_search/binary_search__basic_step1
