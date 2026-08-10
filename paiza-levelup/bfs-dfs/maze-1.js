c248375895cad927336ca5a0e7fd48c484446ac5
Keita Takahashi <takanenokei@gmail.com>
2026-08-10 18:54:05 +0900

    feat: add maze-1 solution

---
+++ paiza-levelup/bfs-dfs/maze-1.js
@@ -0,0 +1,30 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const [h, w] = lines[0].split(' ').map(Number);
+const [y, x] = lines[1].split(' ').map(Number);
+
+const grid = [];
+for (let i = 0; i < h; i++) {
+  grid.push(new Array(w).fill('.'));
+}
+
+const moves = [
+  [0, 0],
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+for (const [dy, dx] of moves) {
+  const ny = y + dy;
+  const nx = x + dx;
+  if (ny >= 0 && ny < h && nx >= 0 && nx < w) {
+    grid[ny][nx] = '*';
+  }
+}
+
+for (let i = 0; i < h; i++) {
+  console.log(grid[i].join(''));
+}
