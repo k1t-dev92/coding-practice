431b14719e8078115b88b3f3ab35c72c6c8988e6
Keita Takahashi <takanenokei@gmail.com>
2026-08-10 13:55:49 +0900

    feat: add max-window-sum-1 solution

---
+++ paiza-levelup/prefix-sum/max-window-sum-1.js
@@ -0,0 +1,17 @@
+const a = [1, 5, 9, 7, 5, 3, 2, 5, 8, 4];
+
+const s = new Array(a.length + 1);
+s[0] = 0;
+
+for (let i = 0; i < a.length; i++) {
+  s[i + 1] = s[i] + a[i];
+}
+
+const results = [];
+for (let i = 0; i <= 7; i++) {
+  results.push(s[i + 3] - s[i]);
+}
+
+console.log(Math.max(...results));
+
+// https://paiza.jp/works/mondai/prefix_sum_problems/prefix_sum_problems__sum_max_step1
