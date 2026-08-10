b07357ddda4831f0de2cbc41ae688d15e947029a
Keita Takahashi <takanenokei@gmail.com>
2026-08-10 13:58:30 +0900

    feat: add array line to input parsing example

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const lines = input.trim().split('\n');
 
 const n = Number(lines[0]);
 const [a, b] = lines[1].split(' ').map(Number);
+const c = lines[2].split(' ').map(Number);
 
 console.log(n);
 console.log(a, b);
+console.log(c);
