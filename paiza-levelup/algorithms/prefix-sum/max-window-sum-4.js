9a4c35e7d442ebe23011350ed40c89a73ad821a3
Keita Takahashi <takanenokei@gmail.com>
2026-08-17 22:10:00 +0900

    refactor: use n for prefix sum array length in max-window-sum-4

--- paiza-levelup/algorithms/prefix-sum/max-window-sum-4.js
+++ paiza-levelup/algorithms/prefix-sum/max-window-sum-4.js
@@ -4,7 +4,7 @@ const lines = input.trim().split('\n');
 const [n, k] = lines[0].split(' ').map(Number);
 const a = lines[1].split(' ').map(Number);
 
-const s = new Array(a.length + 1);
+const s = new Array(n + 1);
 s[0] = 0;
 for (let i = 0; i < n; i++) {
   s[i + 1] = s[i] + a[i];
