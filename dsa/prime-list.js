bd2dd2d3725f95183793e30b291680a189ede424
Keita Takahashi <takanenokei@gmail.com>
2026-08-21 23:59:59 +0900

    style: remove blank lines inside the loop in prime-list

--- dsa/prime-list.js
+++ dsa/prime-list.js
@@ -2,18 +2,16 @@ const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.trim().split('\n');
 
 const n = Number(lines[0]);
-const results = [];
 
+const results = [];
 for (let i = 2; i <= n; i++) {
   let isPrime = true;
-
   for (let j = 2; j * j <= i; j++) {
     if (i % j === 0) {
       isPrime = false;
       break;
     }
   }
-
   if (isPrime) {
     results.push(i);
   }
