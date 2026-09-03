b03ad67bbe238e2467a2bad186e391067792bf07
Keita Takahashi <takanenokei@gmail.com>
2026-09-03 23:36:13 +0900

    feat: add rpn solution

---
+++ dsa/rpn.js
@@ -0,0 +1,26 @@
+const input = require('fs').readFileSync('/dev/stdin', 'utf8');
+const lines = input.trim().split('\n');
+
+const n = Number(lines[0]);
+const a = lines[1].split(' ');
+
+const stack = [];
+for (let i = 0; i < n; i++) {
+  if (a[i] === '+' || a[i] === '-' || a[i] === '*' || a[i] === '/') {
+    const right = stack.pop();
+    const left = stack.pop();
+    if (a[i] === '+') {
+      stack.push(left + right);
+    } else if (a[i] === '-') {
+      stack.push(left - right);
+    } else if (a[i] === '*') {
+      stack.push(left * right);
+    } else {
+      stack.push(left / right);
+    }
+  } else {
+    stack.push(Number(a[i]));
+  }
+}
+
+console.log(stack.pop());
